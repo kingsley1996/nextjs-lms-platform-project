0aa377a0ba94ca77bc89c56f273de6f8893deaf5
samuel <linhdang.dev96@gmail.com>
2024-01-31 01:35:08 +0700

    feature: course-creation-ui-and-set-up-form-data

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 const CoursesPage = () => {
-  return (  
+  return (
     <div>
-      Courses page!
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
     </div>
   );
-}
- 
+};
+
 export default CoursesPage;
