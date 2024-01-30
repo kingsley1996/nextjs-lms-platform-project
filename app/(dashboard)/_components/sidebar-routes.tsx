e1bbfabb8f1ec946bd76f031963fd6e260a070a9
samuel <linhdang.dev96@gmail.com>
2024-01-31 00:58:49 +0700

    feature:dynamic-layout-base-on-routes

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,28 +1,44 @@
 'use client';
 
-import { Compass, Layout } from "lucide-react";
-import { SidebarItem } from "./sidebar-item";
+import { BarChart, Compass, Layout, List } from 'lucide-react';
+import { SidebarItem } from './sidebar-item';
+import { usePathname } from 'next/navigation';
 
 const guestRoutes = [
   {
     icon: Layout,
-    label: "Dashboard",
-    href: "/"
+    label: 'Dashboard',
+    href: '/',
   },
   {
     icon: Compass,
-    label: "Browse",
-    href: "/search"
+    label: 'Browse',
+    href: '/search',
   },
-]
+];
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: 'Courses',
+    href: '/teacher/courses',
+  },
+  {
+    icon: BarChart,
+    label: 'Analytics',
+    href: '/teacher/analytics',
+  },
+];
 
 const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes('/teacher');
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
-  return (  
+  return (
     <div className="flex flex-col w-full">
       {routes.map((route) => (
-        <SidebarItem 
+        <SidebarItem
           key={route.href}
           icon={route.icon}
           label={route.label}
@@ -31,6 +47,6 @@ const SidebarRoutes = () => {
       ))}
     </div>
   );
-}
- 
+};
+
 export default SidebarRoutes;
