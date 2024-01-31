d09221f7650df2c5aae11850d6999b51c08108d5
samuel <linhdang.dev96@gmail.com>
2024-02-01 02:27:33 +0700

    feature:course-description-form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -19,21 +19,26 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Textarea } from '@/components/ui/textarea';
 
-interface TitleFormProsp {
+interface DescriptionFormProsp {
   initialData: {
-    title: string;
+    description: string;
   };
   courseId: string;
 }
 
 const formSchema = z.object({
-  title: z.string().min(1, {
-    message: 'Title is required',
+  description: z.string().min(1, {
+    message: 'Description is required',
   }),
 });
 
-export const TitleForm = ({ initialData, courseId }: TitleFormProsp) => {
+export const DescriptionForm = ({
+  initialData,
+  courseId,
+}: DescriptionFormProsp) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const toggleEdit = () => setIsEditing((current) => !current);
@@ -60,19 +65,28 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProsp) => {
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Course title
+        Course description
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
           ) : (
             <>
               <Pencil className="h-4 w-4 mr-2" />
-              Edit title
+              Edit description
             </>
           )}
         </Button>
       </div>
-      {!isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
+      {!isEditing && (
+        <p
+          className={cn(
+            'text-sm mt-2',
+            !initialData.description && 'text-slate-500 italic'
+          )}
+        >
+          {initialData.description || 'No description'}
+        </p>
+      )}
       {isEditing && (
         <Form {...form}>
           <form
@@ -81,13 +95,13 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProsp) => {
           >
             <FormField
               control={form.control}
-              name="title"
+              name="description"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Input
+                    <Textarea
                       disabled={isSubmitting}
-                      placeholder="e.g. 'Advanced web development'"
+                      placeholder="e.g. 'This course is about...'"
                       {...field}
                     />
                   </FormControl>
