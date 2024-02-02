7648cbf4eca06a6ed42f286d9c5d1b5a16bb4e03
samuel <linhdang.dev96@gmail.com>
2024-02-02 22:00:56 +0700

    feature:chapter-creation-api

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -14,6 +14,7 @@ import { ImageForm } from './_components/image-form';
 import { CategoryForm } from './_components/category-form';
 import { PriceForm } from './_components/price-form';
 import { AttachmentForm } from './_components/attachment-form';
+import { ChaptersForm } from './_components/chapters-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -25,8 +26,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+      userId,
     },
     include: {
+      chapters: {
+        orderBy: {
+          position: 'asc',
+        },
+      },
       attachments: {
         orderBy: {
           createdAt: 'desc',
@@ -34,6 +41,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       },
     },
   });
+
+  if (!course) {
+    return redirect('/');
+  }
 
   const categories = await db.category.findMany({
     orderBy: {
@@ -49,6 +60,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     course?.imageUrl,
     course?.price,
     course?.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
   ];
 
   const totalFields = requiredFields.length;
@@ -94,7 +106,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               <IconBadge icon={ListChecks} />
               <h2 className="text-xl">Course chapters</h2>
             </div>
-            <div>TODO: Chapters </div>
+            <ChaptersForm initialData={course} courseId={course.id} />
           </div>
           <div className="flex items-center gap-x-2">
             <IconBadge icon={CircleDollarSign} />
