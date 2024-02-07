5b24fd0950db1962d19e5f369efa977d7fd3c3c3
samuel <linhdang.dev96@gmail.com>
2024-02-07 19:07:57 +0700

    feature:category-query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,9 +1,18 @@
-const SearchPage = () => {
-  return (  
-    <div>
-      This is a search page!
+import { db } from '@/lib/db';
+import Categories from './_components/categories';
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
     </div>
   );
-}
- 
+};
+
 export default SearchPage;
